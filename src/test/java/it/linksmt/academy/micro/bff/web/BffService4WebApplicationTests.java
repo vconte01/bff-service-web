d46ac0743ad17f8b6158a10eb2460e5c9a866122
CONTEV <vincenzo.conte@linksmt.it>
2022-07-03 20:11:01 +0200

    BFF Sincrono Incompleto

---
+++ src/test/java/it/linksmt/academy/micro/bff/web/BffService4WebApplicationTests.java
@@ -0,0 +1,13 @@
+package it.linksmt.academy.micro.bff.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffService4WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
